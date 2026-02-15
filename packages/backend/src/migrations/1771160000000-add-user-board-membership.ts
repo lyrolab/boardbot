83f1c1dd32b2011620cc285bbf8d880e28d4852a
Samuel Prak <samuel@samuelprak.fr>
2026-02-15 17:24:30 +0100

    fix: migration

--- packages/backend/src/migrations/1771160000000-add-user-board-membership.ts
+++ packages/backend/src/migrations/1771160000000-add-user-board-membership.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class AddUserBoardMembership1771200000000 implements MigrationInterface {
-  name = "AddUserBoardMembership1771200000000"
+export class AddUserBoardMembership1771160000000 implements MigrationInterface {
+  name = "AddUserBoardMembership1771160000000"
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
